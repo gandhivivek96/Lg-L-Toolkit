5186da8093f3ee509eafb637ff8d401000c4b5d6
Joey Rizzoli <io.nolinuxnoparty@gmail.com>
2014-01-25 20:44:55 +0100

    Update Unlock.bat
    fixed issue that cause adb shell asked for root acces and the phone cannot be rooted cuz bootloader is locked, so made adb running as root

--- Lg-l7/Unlock.bat
+++ Lg-l7/Unlock.bat
@@ -6,7 +6,7 @@ adb wait-for-device
 @adb remount
 @adb push C:\LgTool\Lg-l7\bootloader\emmc_appsboot.bin /sddcard/
 echo Writing.....
-@adb shell su -c "dd if=/data/local/tmp/emmc_appsboot.bin of=/dev/block/mmcblk0p5 bs=4096"
+@adb shell dd if=/data/local/tmp/emmc_appsboot.bin of=/dev/block/mmcblk0p5 bs=4096
 @adb reboot
 @adb wait-for device
 @adb shell rm /sdcard/emmc_appsboot.bin
