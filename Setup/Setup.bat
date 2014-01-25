493184b79e30b38ebca6f279e71a12ea90cf95eb
Joey Rizzoli <io.nolinuxnoparty@gmail.com>
2014-01-25 21:54:59 +0100

    Update Setup.bat

--- Setup/Setup.bat
+++ Setup/Setup.bat
@@ -1,48 +1,67 @@
 @echo off
 cls
-
-
-echo MBBG
-echo MBBG
-echo MBBG
-echo MBBG
-echo MBBG
-echo MBBG
-echo MBBG
-echo MBBG
-echo MBBG
-echo MBBG
-echo MBBG
-echo MBBG
-echo MBBG       ToolKit                ----       Setup
-echo MBBG
-echo MBBG                                     By Linuxx
-echo MBBG                                   and
-echo MBBG                             theOwner997
-echo MBBG
-echo MBBG
-echo MBBG
-echo MBBG
-echo MBBGGGGGGGGGGGGGHHHHHIIIIIII
-echo MBBBBBBBBBBBBBBBBGGGGGGGGHHI
-echo MMMMMMMMMMMMMMMMMMMMMMMMMMMI
+color F2
+mode con cols=81
+mode con lines=45
 echo.
+echo    ###########################################################################
+echo    ##                                                                       ## 
+echo    ##                         iN                iZ                          ## 
+echo    ##                          Uo               B                           ## 
+echo    ##                           vLioOBBBBBBMqviq                            ##
+echo    ##                          vMBBBBBBBBBBBBBBBVi                          ## 
+echo    ##                        FBBBBBMOOOOOOOOMBBBBBMi                        ## 
+echo    ##                      iBBBB  iBOGGGGGGGBP  oBBBI                       ## 
+echo    ##                     iBBMMM  iBOGGGGGGGBk  uBMBBN                      ##
+echo    ##                     BBMOOBBBBMOOOOOOOOMBBBBMOMBBv                     ##
+echo    ##                    IBBBBBBBBBBBBBBBBBBBBBBBBBBBBB                     ## 
+echo    ##                    YNUAAAAAAAAAAAAAAAAAAAAAAAAAVG                     ##
+echo    ##              VZPi                                   uZqr              ## 
+echo    ##            iBBBBBF qBBBBBBBBBBBBBBBBBBBBBBBBBBBBB  BBBBBM             ## 
+echo    ##            XBMOMBM uBMMMMMMMMMMMMMMMMMMMMMMMMMMBB iBBOMBB             ## 
+echo    ##            UBOGOBZ JBOGGGGGGGGGGGGGGGGGGGGGGGGOMB iBMGOBB             ## 
+echo    ##            uBOGOBE JBOGGGGGGGGGGGGGGGGGGGGGGGGGMB iBMGOBB             ## 
+echo    ##            UBOGOBE JBOGGGGGGGGGGGGGGGGGGGGGGGGOMB iBMGOBB             ## 
+echo    ##            UBOGOBE JBOGGGGGGGGGGGGGGGGGGGGGGGGOMB iBMGOBB             ## 
+echo    ##            uBOGOBE JBOGGGGGGGGGGGGGGGGGGGGGGGGOMB iBMGOBB             ## 
+echo    ##            UBOGOBE JBOGGGGGGGGGGGGGGGGGGGGGGGGOMB iBMGOBB             ## 
+echo    ##            FBMOOBG JBOGGGGGGGGGGGGGGGGGGGGGGGGOMB iBMOOBB             ## 
+echo    ##            uBBBBBM uBOGGGGGGGGGGGGGGGGGGGGGGGGGMB iBBBBBB             ## 
+echo    ##             FBBBM  VBOGGGGGGGGGGGGGGGGGGGGGGGGGMB  jBBBBi             ## 
+echo    ##                    PBMGGGGGGGGGGGGGGGGGGGGGGGGOBB                     ## 
+echo    ##                    XBBBMMOOGGGGOMMMMOOGGGGOMMMBBB                     ##  
+echo    ##                     PBBBBBMOGGOBBBBBBMOGGOMBBBBBi                     ## 
+echo    ##                          uBOGOBG    rBMGGMB                           ## 
+echo    ##                          vBOGOBG    iBMGGBB                           ## 
+echo    ##                          uBOGOBM    iBMGGBB                           ## 
+echo    ##                          oBMGOBB    iBMGOBB                           ## 
+echo    ##                          XBBMBBB    rBBMMBB                           ## 
+echo    ##                           BBBBBi     MBBBBj                           ## 
+echo    ##                             ii         ii                             ## 
+echo    ##                                                                       ## 
+echo    ##                           Lg L Toolkit Setup                          ##
+echo    ##                                                                       ##
+echo    ##                                                                       ##
+echo    ###########################################################################
 echo.
 pause
+color 2
 cls
-echo Setting up PC...
-@erase C:\LgTool\*
-@erase C:\LgTool
-echo Copying files...
-echo.
 @md C:\LgTool
-@copy Drivers\* C:\LgTool\
-@copy Lg-l3 C:\LgTool\
-@copy Lg-l5 C:\LgTool\
-@copy Lg-l7 C:\LgTool\
-@mkdir C:\LgTool\common
-@copy common\RootC:\LgTool\common\
-@copy common\Backup C:\LgTool\common\
-@copy common\Start.bat C:\LgTool\
+@md C:\LgTool\Common
+@md C:\LgTool\Backup
+@md C:\LgTool\Lg-l3
+@md C:\LgTool\Lg-l5
+@md C:\LgTool\Lg-l7
+@md C:\LgTool\Pulled
+@md C:\LgTool\Pulled\Camera\
+@md C:\LgTool\Common\Root
+@copy Drivers\* C:\Android\
+@copy L3\* C:\LgTool\Lg-l3\
+@copy L5\* C:\LgTool\Lg-l5\
+@copy L7\* C:\LgTool\Lg-l7\
+@copy common\*.bat C:\LgTool\common\
+@copy root\* C:\LgTool\Common\Root\
+@copy common\Start.exe C:\LgTool\
 @echo Done!
-C:\LgTool\Start.bat
+
