05e2abebf86c39dd739c640d638a5c27327af24a
Joey Rizzoli <io.nolinuxnoparty@gmail.com>
2014-01-25 20:45:45 +0100

    Create Unlock.bat

---
+++ Lg-l5/Unlock.bat
@@ -0,0 +1,12 @@
+@echo off
+cls
+adb wait-for-device
+@adb root
+adb wait-for-device
+@adb remount
+@adb push C:\LgTool\Lg-l5\bootloader\emmc_appsboot.bin /sddcard/
+echo Writing.....
+@adb shell dd if=/data/local/tmp/emmc_appsboot.bin of=/dev/block/mmcblk0p5 bs=4096
+@adb reboot
+@adb wait-for device
+@adb shell rm /sdcard/emmc_appsboot.bin
