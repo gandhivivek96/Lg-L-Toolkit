0a3911cf0a47d40158cd95541f456f3e9b7f29a5
Joey Rizzoli <io.nolinuxnoparty@gmail.com>
2014-01-25 21:52:56 +0100

    Update Camera.bat

--- common/Camera.bat
+++ common/Camera.bat
@@ -1,10 +1,10 @@
 @echo off
 cls
 adb wait-for-device
-echo Photos will be pull to C:\LgTool\Pulled
+echo Photos will be pull to C:\LgTool\Pulled\Camera
 echo.
 pause
 echo Copying camera photos...
-@adb pull /sdcard/DCIM C:\LgTool\Pulled\
+@adb pull /sdcard/DCIM/*.jpg C:\LgTool\Pulled\Camera\
 echo Done!
 pause 
