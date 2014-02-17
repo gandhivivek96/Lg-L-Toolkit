800d4a00ff5bedcf27bc3c509ba19ab9b023a0ac
theowner997 <georgelaf997@hotmail.com>
2014-02-17 15:31:59 +0200

    Update Root.bat

--- common/Root.bat
+++ common/Root.bat
@@ -1,12 +1,38 @@
 cls
 @echo off
+:rootwarning
 cls
-adb wait-for-device
+echo.
+echo Rooting requires installed recovery first
+echo.
+echo Did you install recovery?
+echo.
+echo 1. Yes, go ahead and root
+echo 2. No, take me back to menu to install recovery
+echo.
+set /p S= :?
+if %S%==1 goto root
+if %S%==2 :: start.exe
+echo.
+echo Invalid Input? Try again!...
+echo.
+pause 
+goto rootwarning
+
+
+:root
+cls
+@adb wait-for-device
+echo.
 echo This will take about 1 minute (max 2)
-@adb wait-for-device
 @adb reboot recovery
-@adb wait-for-device
-echo Go To Mount&Storage
+echo.
+echo When you boot into Recovery
+echo Unplug and plug in again your USB Cable
+echo Press Enter when ready.
+pause >nul
+echo.
+echo Go To Mount and Storage
 echo Select Mount /system and Mount /data, then
 pause
 @adb shell mount -rw -o,remount /system
@@ -85,3 +111,4 @@ pause
 echo Done!
 @adb reboot
 pause
+goto ::start.exe
